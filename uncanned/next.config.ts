dd04b14ec7d4de4e6fd129a666f209c95be8f2d9
Akanksha Duvvuri <akanksha.duvvuri16@gmail.com>
2026-06-01 23:51:44 +0530

    disable image optimization for static export

--- uncanned/next.config.ts
+++ uncanned/next.config.ts
@@ -1,9 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
-  output: 'export',
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
